4dbcae19f81592d4e291b8b32e3d6b941dbb15b8
iparamsidhu <200554189@student.georgianc.on.ca>
2024-10-17 16:14:55 -0400

    trial

--- Assignment1/express-portfolio/routes/index.js
+++ Assignment1/express-portfolio/routes/index.js
@@ -3,22 +3,27 @@ const router = express.Router();
 
 // Home Page
 router.get('/', (req, res) => {
-    res.render('home', { title: 'Home' });
+  res.render('home', { title: 'Home' });
 });
 
-// About Me Page
+// About Page
 router.get('/about', (req, res) => {
-    res.render('about', { title: 'About Me' });
+  res.render('about', { title: 'About Me' });
 });
 
 // Projects Page
 router.get('/projects', (req, res) => {
-    res.render('projects', { title: 'Projects' });
+  res.render('projects', { title: 'Projects' });
+});
+
+// Resume Page
+router.get('/resume', (req, res) => {
+  res.render('resume', { title: 'Resume' });
 });
 
 // Contact Me Page
 router.get('/contact', (req, res) => {
-    res.render('contact', { title: 'Contact Me' });
+  res.render('contact', { title: 'Contact Me' });
 });
 
 module.exports = router;
