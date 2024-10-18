7a4a7d313b567a2926fd34475a6ff405f2e78050
iparamsidhu <200554189@student.georgianc.on.ca>
2024-10-17 21:04:31 -0400

    Initial commit with basic structure

--- Assignment1/express-portfolio/routes/index.js
+++ Assignment1/express-portfolio/routes/index.js
@@ -1,29 +1,9 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
 
-// Home Page
-router.get('/', (req, res) => {
-  res.render('home', { title: 'Home' });
-});
-
-// About Page
-router.get('/about', (req, res) => {
-  res.render('about', { title: 'About Me' });
-});
-
-// Projects Page
-router.get('/projects', (req, res) => {
-  res.render('projects', { title: 'Projects' });
-});
-
-// Resume Page
-router.get('/resume', (req, res) => {
-  res.render('resume', { title: 'Resume' });
-});
-
-// Contact Me Page
-router.get('/contact', (req, res) => {
-  res.render('contact', { title: 'Contact Me' });
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
